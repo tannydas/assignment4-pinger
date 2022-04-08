bac7701e77f4b166d6278ae539e2e55f1bb046e1
tannydas <79417918+tannydas@users.noreply.github.com>
2022-04-07 21:45:54 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -127,7 +127,8 @@ def ping(host, timeout=1):
     #st_dev = round(statistics.stdev(list(times) * milli_convert, 2))
     st_dev = 3
 
-    vars = [str(round(min(times) * milli_convert, 6)), str(round(sum(times)/len(times) * milli_convert, 6)), str(round(max(times) * milli_convert, 6)), str(st_dev)]
+    #vars = [str(round(min(times) * milli_convert, 6)), str(round(sum(times)/len(times) * milli_convert, 6)), str(round(max(times) * milli_convert, 6)), str(st_dev)]
+
 
     print(vars)
     return vars
